c9e981af0d2d5f6ba4f39d2274c3dde1f23afc39
DannyAAM <danny@saru.moe>
2024-10-05 05:30:51 +0800

    fix syntax for sh

--- copy-files.sh
+++ copy-files.sh
@@ -2,7 +2,7 @@
 cd $( dirname -- "$0"; )
 list=".vimrc"
 for i in $list; do
-    if [[ -d $i ]]; then
+    if [ -d $i ]; then
         rsync -auK $i/. "$(readlink -f ~/$i)"
     else
         rsync -auK $i "$(readlink -f ~/$i)"
